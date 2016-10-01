c49177420306f3ac57aaf492c85fa00e63a5e47c
Andrew H <FlamesRunner@users.noreply.github.com>
2016-10-01 00:17:47 -0400

    added session_destroy for extra security

--- web/logout.php
+++ web/logout.php
@@ -1,9 +1,22 @@
 <?php
 session_start();
+
+// make sure the session variables have been emptied
+
 $_SESSION['username'] = "";
 $_SESSION['rank'] = "";
+
+// now unset them
+
 unset($_SESSION['username']);
 unset($_SESSION['rank']);
+
+// and finally permanent destruction
+
+session_destroy();
 session_write_close();
+
+// see you later!
+
 header("Location: /");
 ?>
