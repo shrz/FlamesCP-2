8a9afaa84e6424ef3af44b885bfab040fb9cba7b
Andrew H <FlamesRunner@users.noreply.github.com>
2016-10-01 19:41:27 -0400

    Updated to check if a user actually exists

--- web/include/login.php
+++ web/include/login.php
@@ -1,9 +1,25 @@
 <?php
 session_start();
 
-if (isset($_SESSION['username'])){
-$logged_in = 'true';
+require 'include/db.php';
+
+if (empty($_SESSION['username'])){
+  
 } else {
-$logged_in = 'false';
+
+$user_query = $conn->prepare('select id from login where username=:username');
+$user_query->bindParam(':username', $_SESSION['username']);
+$user_query->execute();
+  
+if (!$user_query->rowCount() > 0){
+
+// the user set in the session is not valid
+// log out now
+
+header('Location: /logout.php');
+die('');
+  
+}  
+  
 }
 ?>
