befe80c09a5414e8669ade19728b5b96c24ca154
Andre Bongon <andremarx13@gmail.com>
2016-10-02 12:31:12 +0900

    Update installer.php

--- web/installer.php
+++ web/installer.php
@@ -12,18 +12,24 @@ require 'include/header.php';
 <div class="jumbotron">
 <h2 style="text-align: center"><b>Welcome</b></h2>
 <?php
-if (!empty($_POST['password'])){
-$password = crypt($_POST['password'], $salt);
-$query = $conn->prepare('insert into login (username, password, status) VAlUES (:username, :password, "admin");');
-$query->bindParam(':username', 'admin');
-$query->bindParam(':password', password_hash($_POST['password'], PASSWORD_BCRYPT, $bcrypt_opt));
-
-if($query->execute()){
-//destroy installer since installation was successful
-$msg = 'success';
+if (!empty($_POST['password']) && !empty($_POST['conf_password'])){
+  if($_POST['password'] === $_POST['conf_password']) {
+    $password = crypt($_POST['password'], $salt);
+    $query = $conn->prepare('insert into login (username, password, status) VAlUES (:username, :password, "admin");');
+    $query->bindParam(':username', 'admin');
+    $query->bindParam(':password', password_hash($_POST['password'], PASSWORD_BCRYPT, $bcrypt_opt));
+  
+    if ($query->execute()){
+      //destroy installer since installation was successful
+      $msg = 'success';
+     } else {
+      $msg = 'fail';
+     }
+  } else {
+    $msg = 'fail';
+  }
 } else {
-$msg = 'fail';
-}
+  $msg = 'fail';
 }
 ?>
 <p style="text-align: center">Please set your password.</p>
@@ -40,6 +46,8 @@ echo '<br /><div class="alert alert-success"><b>Error:</b> The creation of the a
 <input class="form-control" value="admin">
 <label>Password</label>
 <input class="form-control" placeholder="Password..." type="password" name="password">
+<label>Confirm Password</label>
+<input class="form-control" placeholder="Password..." type="password" name="conf_password">
 </form>
 <?php
 }
