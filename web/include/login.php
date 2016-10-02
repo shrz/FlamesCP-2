fcd4fb4a4860c2a26bdf7aaf874bc8a74567e616
Andre Bongon <andremarx13@gmail.com>
2016-10-02 11:27:09 +0800

    Update login.php (#20)

--- web/include/login.php
+++ web/include/login.php
@@ -3,23 +3,20 @@ session_start();
 
 require 'include/db.php';
 
-if (empty($_SESSION['username'])){
+if (!empty($_SESSION['username'])) {
+  $user_query = $conn->prepare('select id from login where username=:username');
+  $user_query->bindParam(':username', $_SESSION['username']);
+  $user_query->execute();
   
-} else {
+  if (!$user_query->rowCount() > 0){
 
-$user_query = $conn->prepare('select id from login where username=:username');
-$user_query->bindParam(':username', $_SESSION['username']);
-$user_query->execute();
+    // the user set in the session is not valid
+    // log out now
+
+    header('Location: /logout.php');
+    die('');
   
-if (!$user_query->rowCount() > 0){
-
-// the user set in the session is not valid
-// log out now
-
-header('Location: /logout.php');
-die('');
-  
-}  
+  }  
   
 }
 ?>
